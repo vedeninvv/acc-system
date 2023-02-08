94c41e0fa5fd34ae24dfc5bba7e0f405910a3839
Vadim Vedenin <vedeninvv@mail.ru>
2023-02-08 10:17:45 +0300

    init project

---
+++ src/main/java/com/example/accsystem/AccSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.accsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccSystemApplication.class, args);
+    }
+
+}
