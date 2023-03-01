7fe9a8d8b2f89adbedf8874ce00ed1410b1bd194
Vadim Vedenin <vedeninvv@mail.ru>
2023-03-01 09:40:26 +0300

    Настройка проекта: Изменил имя пакета и структуру pom.xml

--- src/main/java/com/practice/accsystem/AccSystemApplication.java
+++ src/main/java/com/practice/accsystem/AccSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.accsystem;
+package com.practice.accsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
