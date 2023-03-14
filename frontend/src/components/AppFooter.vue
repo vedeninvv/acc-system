2feb56c9a910f80c51fc3235aa52e9a042073f2f
Vadim Vedenin <vedeninvv@mail.ru>
2023-03-14 22:28:49 +0300

    Добавил навигацию на фронтенде
    Добавил меню, сделал роутинг, создал пустые страницы под пункты меню, добавил footer

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer min-height="50px" class="text-center flex-column">
+    <div class="py-2">2023 - НИЦ СПб ЭТУ - ЦРТРИС</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ContractsPage from "@/views/ContractsPage";
+import CounterpartiesPage from "@/views/СounterpartiesPage"
+import ReportsPage from "@/views/ReportsPage";
+import AdministrationPage from "@/views/AdministrationPage";
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+    {
+        path: '/',
+        redirect: '/contracts'
+    },
+    {
+        path: '/contracts',
+        component: ContractsPage
+    },
+    {
+        path: '/counterparties',
+        component: CounterpartiesPage
+    },
+    {
+        path: '/reports',
+        component: ReportsPage
+    },
+    {
+        path: '/administration',
+        component: AdministrationPage
+    }
+]
 
 const router = new VueRouter({
     mode: 'history',
