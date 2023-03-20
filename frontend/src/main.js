7bd8e79d0159d8561fd2967ded66e58736fc35da
Vadim Vedenin <vedeninvv@mail.ru>
2023-03-20 22:56:47 +0300

    Добавил страницу для логина

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
+const accessToken = localStorage.getItem('accessToken')
+if (accessToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+}
+
 new Vue({
-  vuetify,
-  router,
-  render: h => h(App)
+    vuetify,
+    router,
+    render: h => h(App)
 }).$mount('#app')
