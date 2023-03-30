b7e47996a2bee5e81e5039d24aba6b6d61832f6f
Vadim Vedenin <vedeninvv@mail.ru>
2023-03-30 18:49:25 +0300

    Удалил неиспользуемые импорты для классов тестов

--- backend/src/test/java/com/practice/accsystem/service/ContractServiceImplTest.java
+++ backend/src/test/java/com/practice/accsystem/service/ContractServiceImplTest.java
@@ -20,7 +20,8 @@ import org.springframework.data.domain.PageRequest;
 import java.math.BigDecimal;
 import java.util.*;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
