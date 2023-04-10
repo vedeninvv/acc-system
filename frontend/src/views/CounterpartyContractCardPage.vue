d51735cd1fbf10ea90dd509fdee7a460de52dd7f
Vadim Vedenin <vedeninvv@mail.ru>
2023-04-10 19:31:18 +0300

    Добавил страницу с формой редактирования/создания договора с контрагентом

---
+++ frontend/src/views/CounterpartyContractCardPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <h1 v-if="isNewContract">Новый договор с контрагентом</h1>
+    <h1 v-else>№{{ counterpartyContractId }} Договор с контрагентом</h1>
+
+    <v-container>
+      <counterparty-contract-form :contract-id="contractId"
+                                  :counterparty-contract-id="counterpartyContractId">
+      </counterparty-contract-form>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import CounterpartyContractForm from "@/components/CounterpartyContractForm";
+
+export default {
+  name: "CounterpartyContractCardPage",
+  components: {CounterpartyContractForm},
+
+  created() {
+    this.contractId = +this.$route.params.contractId
+
+    let urlCounterpartyContractNew = new RegExp("/contracts/(\\d+)/counterparty-contracts/new")
+    if (!urlCounterpartyContractNew.test(this.$route.path)) {
+      this.counterpartyContractId = +this.$route.params.id
+    }
+  },
+
+  data: () => ({
+    contractId: null,
+    counterpartyContractId: null,
+  }),
+
+  computed: {
+    isNewContract() {
+      return this.contractId == null
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
