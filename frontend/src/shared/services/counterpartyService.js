2d95b5c0a2ff95f93ec2b4fb36d21104700ba952
Vadim Vedenin <vedeninvv@mail.ru>
2023-04-12 10:16:19 +0300

    Добавил страницу контрагентов: поиск, добавление, изменение, удаление

--- frontend/src/shared/services/counterpartyService.js
+++ frontend/src/shared/services/counterpartyService.js
@@ -1,5 +1,15 @@
 import {httpClient} from "@/shared/request";
 
+export const apiCreateCounterparty = (counterparty) => new Promise((resolve, reject) => {
+    httpClient.post(`/counterparties`, counterparty)
+        .then(resp => {
+            resolve(resp.data)
+        })
+        .catch(err => {
+            reject(err)
+        })
+})
+
 export const apiGetAllCounterparties = (params) => new Promise((resolve, reject) => {
     httpClient.get(`/counterparties`, {
         params: params
@@ -14,6 +24,26 @@ export const apiGetAllCounterparties = (params) => new Promise((resolve, reject)
 
 export const apiGetCounterpartyById = (counterpartyId) => new Promise((resolve, reject) => {
     httpClient.get(`/counterparties/${counterpartyId}`)
+        .then(resp => {
+            resolve(resp.data)
+        })
+        .catch(err => {
+            reject(err)
+        })
+})
+
+export const apiUpdateCounterpartyById = (counterpartyId, counterparty) => new Promise((resolve, reject) => {
+    httpClient.put(`/counterparties/${counterpartyId}`, counterparty)
+        .then(resp => {
+            resolve(resp.data)
+        })
+        .catch(err => {
+            reject(err)
+        })
+})
+
+export const apiDeleteCounterpartyById = (counterpartyId) => new Promise((resolve, reject) => {
+    httpClient.delete(`/counterparties/${counterpartyId}`)
         .then(resp => {
             resolve(resp.data)
         })
---
+++ frontend/src/views/CounterpartyCardPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <h1 v-if="isNewCounterparty">Новый контрагент</h1>
+    <h1 v-else>№{{ counterpartyId }} Контрагент</h1>
+
+    <v-container>
+      <counterparty-form :counterparty-id="counterpartyId">
+      </counterparty-form>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import CounterpartyForm from "@/components/CounterpartyForm";
+
+export default {
+  name: "CounterpartyCardPage",
+  components: {CounterpartyForm},
+
+  created() {
+    if (this.$route.path !== "/counterparties/new") {
+      this.counterpartyId = +this.$route.params.id
+    }
+  },
+
+  data: () => ({
+    counterpartyId: null,
+  }),
+
+  computed: {
+    isNewCounterparty() {
+      return this.counterpartyId == null
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
