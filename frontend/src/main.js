ce42d7541895a89aa3a1b7061fa25dc2cf68262e
Vadim Vedenin <vedeninvv@mail.ru>
2023-04-21 09:45:42 +0300

    Исправил неотображение иконок без интеренета: иконки теперь устанавливаются локально

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false
 
